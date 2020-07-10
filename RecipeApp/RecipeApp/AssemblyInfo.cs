591c62d550bb77d1e220b517937f9b3841253867
Nathalie Hochscheid <nhochscheid@web.de>
2020-07-10 12:19:34 +0200

    Add floating action button with Font Awesome icon

--- RecipeApp/RecipeApp/AssemblyInfo.cs
+++ RecipeApp/RecipeApp/AssemblyInfo.cs
@@ -1,3 +1,9 @@
+using System.Resources;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: ExportFont("fa-regular-400.ttf", Alias = "FA-R")]
+[assembly: ExportFont("fa-solid-900.ttf", Alias = "FA-S")]
+[assembly: ExportFont("fa-brands-400.ttf", Alias = "FA-B")]
---
+++ RecipeApp/RecipeApp/Controls/FloatingActionButton.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RecipeApp.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FloatingActionButton : Button
+    {
+        public FloatingActionButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
