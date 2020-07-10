19a844d209fb2dbe9550ded8dbae4a9e6f9e30ff
Nathalie Hochscheid <nhochscheid@web.de>
2020-07-10 11:48:17 +0200

    Add recipe list view and view model

---
+++ RecipeApp/RecipeApp/ViewModels/RecipeListViewModel.cs
@@ -0,0 +1,80 @@
+﻿using RecipeApp.Models;
+using RecipeApp.Services;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RecipeApp.ViewModels
+{
+    public class RecipeListViewModel : BindableObject
+    {
+        public RecipeListViewModel(IRecipeService recipeService)
+        {
+            RecipeService = recipeService;
+            Recipes = new ObservableCollection<Recipe>();
+        }
+
+        public bool IsLoading
+        {
+            get
+            {
+                return isLoading;
+            }
+            set
+            {
+                if (isLoading != value)
+                {
+                    isLoading = value;
+                    OnPropertyChanged(nameof(IsLoading));
+                    OnPropertyChanged(nameof(IsLoaded));
+                }
+            }
+        }
+
+        private bool isLoading;
+
+        public bool IsLoaded
+        {
+            get
+            {
+                return !IsLoading;
+            }
+        }
+
+        public ObservableCollection<Recipe> Recipes
+        {
+            get { return recipes; }
+
+            set
+            {
+                if (recipes != value)
+                {
+                    recipes = value;
+                    OnPropertyChanged(nameof(Recipes));
+                }
+            }
+        }
+
+        private ObservableCollection<Recipe> recipes;
+
+        private IRecipeService RecipeService { get; set; }
+
+        public async Task Load()
+        {
+            IsLoading = true;
+
+            Recipes = new ObservableCollection<Recipe>(await LoadRecipes());
+
+            IsLoading = false;
+        }
+
+        private Task<List<Recipe>> LoadRecipes()
+        {
+            return Task.Run(() =>
+            {
+                return RecipeService.GetRecipesAsync();
+            });
+        }
+    }
+}
---
+++ RecipeApp/RecipeApp/Views/RecipeListPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using RecipeApp.Services;
+using RecipeApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RecipeApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RecipeListPage : ContentPage
+    {
+        public RecipeListPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var recipeListViewModel = new RecipeListViewModel(new RecipeService());
+            BindingContext = recipeListViewModel;
+            await recipeListViewModel.Load();
+        }
+    }
+}
